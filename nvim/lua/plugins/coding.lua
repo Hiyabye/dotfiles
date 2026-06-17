5219113af3ed5e7b033fe815442c934000f19c1c
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-17 12:37:22 +0900

    Add LSP configuration and integrate conform.nvim

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,3 +1,60 @@
 return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- Lua language server configuration.
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = {
+                vim.env.VIMRUNTIME,
+              },
+            },
+          },
+        },
+      })
+
+      vim.lsp.enable({
+        "lua_ls",
+        "clangd",
+        "pyright",
+      })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(event)
+          local opts = { buffer = event.buf }
+
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        end,
+      })
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        python = { "black" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+      },
+    },
+  },
+
   { import = "plugins.custom" },
 }
