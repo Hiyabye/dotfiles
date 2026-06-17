2675e4f533dcadc0dce4d59de7801b1364c2bdeb
Jihoon Lee <hiyabye0465@gmail.com>
2026-06-17 12:46:17 +0900

    Fix indentation settings in options.lua for consistency

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,7 +9,9 @@ vim.o.termguicolors = true
 vim.o.signcolumn = "yes"
 vim.o.cursorline = true
 
+vim.o.expandtab = true
 vim.o.tabstop = 2
+vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.smartindent = true
